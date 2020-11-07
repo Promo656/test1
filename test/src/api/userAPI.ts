4210f7d74fce56653574b3d31c7ea35a0e8479d7
Egor <rank2007@mail.ru>
2020-11-07 20:11:40 +0300

    commit

--- test/src/api/userAPI.ts
+++ test/src/api/userAPI.ts
@@ -8,6 +8,7 @@ export const userAPI = {
     auth(username: string, password: string) {
         return instance.post(`api-token-auth/`, {username, password})
             .then(response => {
+                localStorage.setItem('token', response.data.token)
                 return response.data
             })
     },
@@ -19,3 +20,23 @@ export const userAPI = {
     }
 }
 
+async function setAuthHeader(config: any) {
+    let token = localStorage.getItem('token');
+    if (!!token) {
+        config.headers['Authorization'] =`Token:${token}`;
+    }
+
+}
+
+const setInterceptors = (instance: any) => {
+    instance.interceptors.request.use(
+        async (config: any) => {
+            await setAuthHeader(config);
+            return config;
+        },
+        (error: any) => Promise.reject(error)
+    );
+
+}
+
+setInterceptors(instance)
